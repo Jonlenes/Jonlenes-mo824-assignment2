449d1d8e24eb540b2d6cae562684d91410e7bc16
Jonlenes Silva Castro <jonlenes.castro@minerva.local>
2020-10-05 10:24:04 -0300

    Reading instances code

---
+++ src/read_instance.py
@@ -0,0 +1,22 @@
+import json
+import numpy as np
+from glob import glob
+
+
+def list_avaliable_instances(regex="data/*.json"):
+    return glob(regex)
+
+
+def read_json(filename):
+    with open(filename, "r") as file:
+        data = json.loads(file.read())
+    return data
+
+
+def load_instance(filename):
+    json_ins = read_json(filename)
+    return [
+        json_ins["num_vertices"],
+        json_ins["poses"],
+        json_ins["dists"]
+    ]
